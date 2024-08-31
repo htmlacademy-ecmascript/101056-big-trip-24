9f6eb2d59fe9a2db2910b7c77cee3c611c82e5af
Anton Yushkov <a.s.yushkov@bk.ru>
2024-08-31 23:29:56 +0300

    module1-task3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import NewTripInfoView from './view/new-trip-info-view.js';
 import {render} from './render.js';
 
-const tripMainElement = document.querySelector('.trip-main');
+import NewTripInfoView from './view/new-trip-info-view.js';
+import NewTripFiltersView from './view/new-filters-view.js';
+import BoardPresenter from './presenter/board-presenter.js';
+// import NewEventsMessageView from './view/new-events-message-view.js';
 
-render (new NewTripInfoView(), tripMainElement, 'AFTERBEGIN');
+
+const tripMainContainer = document.querySelector('.trip-main');
+const tripFiltersContainer = tripMainContainer.querySelector('.trip-controls__filters');
+const tripEventsContainer = document.querySelector('.trip-events');
+const boardPresenter = new BoardPresenter({container: tripEventsContainer});
+
+
+render (new NewTripInfoView(), tripMainContainer, 'AFTERBEGIN');
+render (new NewTripFiltersView(), tripFiltersContainer);
+// render (new NewEventsMessageView(), tripEventsContainer);
+
+boardPresenter.init();
