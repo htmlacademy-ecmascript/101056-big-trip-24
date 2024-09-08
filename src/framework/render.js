fe1385510f8708ba3a2882ca180490e18befdb7e
Keks <keks@htmlacademy.ru>
2024-09-08 16:23:03 +0300

    :cat:

--- src/framework/render.js
+++ src/framework/render.js
@@ -1,24 +1,80 @@
+import AbstractView from './view/abstract-view.js';
+
+/** @enum {string} Перечисление возможных позиций для отрисовки */
 const RenderPosition = {
-  BEFOREBEGIN: 'beforebegin', // вставить элемент перед началом контейнера
-  AFTERBEGIN: 'afterbegin', // вставить элемент сразу после начала контейнера
-  BEFOREEND: 'beforeend', // вставить элемент перед концом контейнера
-  AFTEREND: 'afterend', // вставить элемент сразу после конца контейнера
+  BEFOREBEGIN: 'beforebegin',
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
 };
 
-function createElement(template) { // принимает template в виде html кода
-  const newElement = document.createElement('div'); // создается новый `div`
-  newElement.innerHTML = template; // устанавливается содержимое этого `div`
+/**
+ * Функция для создания элемента на основе разметки
+ * @param {string} template Разметка в виде строки
+ * @returns {HTMLElement} Созданный элемент
+ */
+function createElement(template) {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
 
-  return newElement.firstElementChild; // возвращается первый дочерний элемент созданного `div`
+  return newElement.firstElementChild;
 }
 
-/*
-component - объект компонента, который должен иметь метод `getElement()`, возвращающий элемент DOM
-container - элемент DOM, в который будет вставлен новый элемент
-place - позиция, где будет вставлен элемент (по умолчанию `BEFOREEND`)
-*/
+/**
+ * Функция для отрисовки элемента
+ * @param {AbstractView} component Компонент, который должен был отрисован
+ * @param {HTMLElement} container Элемент в котором будет отрисован компонент
+ * @param {string} place Позиция компонента относительно контейнера. По умолчанию - `beforeend`
+ */
 function render(component, container, place = RenderPosition.BEFOREEND) {
-  container.insertAdjacentElement(place, component.getElement());
+  if (!(component instanceof AbstractView)) {
+    throw new Error('Can render only components');
+  }
+
+  if (container === null) {
+    throw new Error('Container element doesn\'t exist');
+  }
+
+  container.insertAdjacentElement(place, component.element);
 }
 
-export {RenderPosition, createElement, render};
+/**
+ * Функция для замены одного компонента на другой
+ * @param {AbstractView} newComponent Компонент, который нужно показать
+ * @param {AbstractView} oldComponent Компонент, который нужно скрыть
+ */
+function replace(newComponent, oldComponent) {
+  if (!(newComponent instanceof AbstractView && oldComponent instanceof AbstractView)) {
+    throw new Error('Can replace only components');
+  }
+
+  const newElement = newComponent.element;
+  const oldElement = oldComponent.element;
+
+  const parent = oldElement.parentElement;
+
+  if (parent === null) {
+    throw new Error('Parent element doesn\'t exist');
+  }
+
+  parent.replaceChild(newElement, oldElement);
+}
+
+/**
+ * Функция для удаления компонента
+ * @param {AbstractView} component Компонент, который нужно удалить
+ */
+function remove(component) {
+  if (component === null) {
+    return;
+  }
+
+  if (!(component instanceof AbstractView)) {
+    throw new Error('Can remove only components');
+  }
+
+  component.element.remove();
+  component.removeElement();
+}
+
+export {RenderPosition, createElement, render, replace, remove};
