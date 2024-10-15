be187d724eeba1678c9923a1df5dee498e361780
Anton Yushkov <a.s.yushkov@bk.ru>
2024-10-15 18:16:52 +0300

    отимизировал коннектор

--- src/model/events-connector.js
+++ src/model/events-connector.js
@@ -11,55 +11,49 @@ export default class EventsConnector {
   #clonedOffersMap = structuredClone(this.#offersMap);
   #eventsList = convertKeysToCamelCase(getRandomEvents(EVENTS_COUNT, this.#destinationsList, this.#clonedOffersMap));
 
-  #initializeOffersMap(data) {
+  #initializeOffersMap (data) {
     return data.reduce((offersMap, { type, offers }) => {
-      const offersById = offers.reduce((map, { id, title, price }) => {
-        map.set(id, { title, price, isActive: false });
-        return map;
-      }, new Map());
-
+      const offersById = new Map(
+        offers.map(({ id, title, price }) => [id, { title, price, isActive: false }])
+      );
       offersMap.set(type, offersById);
       return offersMap;
     }, new Map());
   }
 
-  #updateOffersActivity() {
+  #updateOffersActivity () {
     this.#eventsList.forEach(({ offers: eventOffers, type }) => {
       const offersById = this.#clonedOffersMap.get(type);
-
       eventOffers.forEach((offerId) => {
-        if (offersById.has(offerId)) {
-          offersById.get(offerId).isActive = true;
+        const offer = offersById.get(offerId);
+        if (offer) {
+          offer.isActive = true;
         }
       });
     });
   }
 
-  constructor() {
+  constructor () {
     this.#updateOffersActivity();
   }
 
-  #getOfferById(type, id) {
+  #getOfferById (type, id) {
     const offer = this.#clonedOffersMap.get(type)?.get(id);
     return offer ? { id, ...offer } : null;
   }
 
-  #createUserEvents() {
+  #createUserEvents () {
     const destinationsMap = new Map(this.#destinationsList.map(({ id, ...rest }) => [id, rest]));
 
     return Array.from(this.#eventsList.map((event) => {
       const destinationData = destinationsMap.get(event.destination);
-      const offersMap = new Map(event.offers.map((offerId) => {
-        const offer = this.#getOfferById(event.type, offerId);
-        return offer ? [offer.id, offer] : null;
-      }).filter(Boolean));
+      const offersMap = new Map(
+        event.offers.map((offerId) => this.#getOfferById(event.type, offerId)).filter(Boolean).map((offer) => [offer.id, offer])
+      );
 
       return {
         ...event,
-        destination: {
-          id: destinationData.id,
-          ...destinationData
-        },
+        destination: { id: destinationData.id, ...destinationData },
         offers: offersMap
       };
     }).reduce((acc, event) => {
@@ -79,5 +73,4 @@ export default class EventsConnector {
   get offersMap () {
     return this.#offersMap;
   }
-
 }
