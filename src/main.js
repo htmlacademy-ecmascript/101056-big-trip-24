5ab86f842ef9473d61f2a48e3332b4f00d71e7e0
Anton Yushkov <a.s.yushkov@bk.ru>
2024-10-17 18:01:08 +0300

    7.8

--- src/main.js
+++ src/main.js
@@ -1,25 +1,35 @@
 import { render } from './framework/render.js';
 import EventsModel from './model/events-model.js';
+import FilterModel from './model/filter-model.js';
 import NewTripInfoView from './view/new-trip-info-view.js';
 import NewTripFiltersView from './view/new-filters-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
-import { generateFilter } from './mock/filter.js';
 
+const filters = [
+  {
+    type: 'all',
+    count: 0,
+  },
+];
 
 const tripMainContainer = document.querySelector('.trip-main');
 const tripFiltersContainer = tripMainContainer.querySelector('.trip-controls__filters');
 const tripEventsContainer = document.querySelector('.trip-events');
 
 const eventsModel = new EventsModel();
+const filterModel = new FilterModel();
 const boardPresenter = new BoardPresenter({
   container: tripEventsContainer,
   eventsModel
 });
 
-const filters = generateFilter(eventsModel.userEvents);
 
 render (new NewTripInfoView(), tripMainContainer, 'AFTERBEGIN');
-render (new NewTripFiltersView({filters}), tripFiltersContainer);
+render(new NewTripFiltersView({
+  filters,
+  currentFilterType: 'all',
+  onFilterTypeChange: () => {}
+}), tripFiltersContainer);
 
 
 boardPresenter.init();
