76d42a4f85b7bf8b1b6a725d430bd434c174916b
Anton Yushkov <a.s.yushkov@bk.ru>
2024-10-20 17:46:15 +0300

    8.1 класс для работы с API

---
+++ src/events-api-service.js
@@ -0,0 +1,26 @@
+import ApiService from './framework/api-service.js';
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class EventsApiService extends ApiService {
+  get userEvents() {
+    return this._load({url: 'big-trip/points'})
+      .then(ApiService.parseResponse);
+  }
+
+  async updateEvent(event) {
+    const response = await this._load({
+      url: `big-trip/points/${event.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(event),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  }
+}
